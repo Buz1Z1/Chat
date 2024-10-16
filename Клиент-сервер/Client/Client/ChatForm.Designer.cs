81b404bb81ecb892174b4d103b0d24f3b6e0f8f5
Buz1Z1 <stepan-radostev54@yandex.ru>
2024-10-16 17:21:41 +0500

    Add files via upload

---
+++ Клиент-сервер/Client/Client/ChatForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Client
+{
+    partial class ChatForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+       
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatForm));
+            this.enterChat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nicknameData = new System.Windows.Forms.TextBox();
+            this.chatBox = new System.Windows.Forms.RichTextBox();
+            this.userList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.messageData = new System.Windows.Forms.TextBox();
+            this.userMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nameData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterChat
+            // 
+            this.enterChat.Enabled = false;
+            this.enterChat.Location = new System.Drawing.Point(423, 5);
+            this.enterChat.Name = "enterChat";
+            this.enterChat.Size = new System.Drawing.Size(111, 29);
+            this.enterChat.TabIndex = 0;
+            this.enterChat.Text = "Подключится";
+            this.enterChat.UseVisualStyleBackColor = true;
+            this.enterChat.Click += new System.EventHandler(this.enterChat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите ваш НИК:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nicknameData
+            // 
+            this.nicknameData.Enabled = false;
+            this.nicknameData.Location = new System.Drawing.Point(122, 10);
+            this.nicknameData.Name = "nicknameData";
+            this.nicknameData.Size = new System.Drawing.Size(272, 20);
+            this.nicknameData.TabIndex = 2;
+            // 
+            // chatBox
+            // 
+            this.chatBox.BackColor = System.Drawing.Color.White;
+            this.chatBox.Enabled = false;
+            this.chatBox.Location = new System.Drawing.Point(7, 53);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.ReadOnly = true;
+            this.chatBox.Size = new System.Drawing.Size(644, 291);
+            this.chatBox.TabIndex = 3;
+            this.chatBox.Text = "";
+            // 
+            // userList
+            // 
+            this.userList.Enabled = false;
+            this.userList.FormattingEnabled = true;
+            this.userList.Location = new System.Drawing.Point(657, 53);
+            this.userList.Name = "userList";
+            this.userList.Size = new System.Drawing.Size(174, 290);
+            this.userList.TabIndex = 4;
+            this.userList.SelectedIndexChanged += new System.EventHandler(this.userList_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Чат: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(681, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Список пользователей: ";
+            // 
+            // messageData
+            // 
+            this.messageData.Enabled = false;
+            this.messageData.Location = new System.Drawing.Point(14, 365);
+            this.messageData.Name = "messageData";
+            this.messageData.Size = new System.Drawing.Size(817, 20);
+            this.messageData.TabIndex = 7;
+            this.messageData.KeyUp += new System.Windows.Forms.KeyEventHandler(this.messageData_KeyUp);
+            // 
+            // userMenu
+            // 
+            this.userMenu.Name = "userMenu";
+            this.userMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // nameData
+            // 
+            this.nameData.AutoSize = true;
+            this.nameData.Location = new System.Drawing.Point(12, 349);
+            this.nameData.Name = "nameData";
+            this.nameData.Size = new System.Drawing.Size(35, 13);
+            this.nameData.TabIndex = 9;
+            this.nameData.Text = "label4";
+            this.nameData.Click += new System.EventHandler(this.nameData_Click);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 396);
+            this.Controls.Add(this.nameData);
+            this.Controls.Add(this.messageData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userList);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.nicknameData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enterChat);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ChatForm";
+            this.Text = "Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChatForm_FormClosing);
+            this.Load += new System.EventHandler(this.ChatForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterChat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nicknameData;
+        private System.Windows.Forms.RichTextBox chatBox;
+        private System.Windows.Forms.ListBox userList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox messageData;
+        private System.Windows.Forms.ContextMenuStrip userMenu;
+        private System.Windows.Forms.Label nameData;
+    }
+}
+
